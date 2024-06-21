baa7d8b5a06638a5c812cdae46e2a68009493691
jlsi3001 <88526263+jlsi3001@users.noreply.github.com>
2024-06-21 01:24:55 -0500

    Comentario sobre código inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,6 +4,9 @@ public record PersonaRegistro(String nombre, String apellidos) {
 
     @Override
     public String toString() {
+        //José, comenté la línea de código que considero se puede eliminar
+        //System.out.println("Esta instrucción es inservible");    
         return "%s %s".concat(nombre + apellidos);
     }
 }
+
